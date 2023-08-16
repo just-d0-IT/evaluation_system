4e427f97a03afd3118973993d52ea1bedffb64de
ovo <757806635@qq.com>
2023-08-17 07:47:48 +0800

    Initial commit

---
+++ src/test/java/com/zhixinsiwei/evaluation_system/EvaluationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhixinsiwei.evaluation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
